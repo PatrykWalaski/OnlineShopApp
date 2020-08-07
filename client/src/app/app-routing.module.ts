4198cc4407b83bb9c09177e27fb2c19a0c980c3c
PatrykWalaski <patrykwalaski@gmail.com>
2020-08-07 18:28:17 +0200

    Added routing and lazy loading in client

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShopComponent } from './shop/shop.component';
+import { ProductDetailsComponent } from './shop/product-details/product-details.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  // shop  module is only activated when user accessed the shop part of the application
+  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)}, 
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
