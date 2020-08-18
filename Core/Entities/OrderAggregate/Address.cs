6e32d93abfef10e2d103fb3b8dfb06a57a1e32a2
PatrykWalaski <patrykwalaski@gmail.com>
2020-08-18 17:29:28 +0200

    Added orders func in API; Aggregate and Unit Of Work pattern

---
+++ Core/Entities/OrderAggregate/Address.cs
@@ -0,0 +1,25 @@
+namespace Core.Entities.OrderAggregate
+{
+    public class Address
+    {
+        //entity framework needs parameterless constructor
+        public Address()
+        {
+        }
+
+        public Address(string firstName, string lastName, string street, string city, string zipcode)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Street = street;
+            City = city;
+            Zipcode = zipcode;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+    }
+}
